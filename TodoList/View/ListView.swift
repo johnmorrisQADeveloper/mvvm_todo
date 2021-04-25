c37353308947b0e837673a234650592fae8763fe
john morris <fennymorris@live.co.uk>
2021-04-25 17:36:59 +0100

    updated one of the tash

--- TodoList/View/ListView.swift
+++ TodoList/View/ListView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ListView: View {
     @State var items: [ItemModel] = [
         ItemModel(title: "This is the first title!", isCompleted: false),
-        ItemModel(title: "This is the second title!", isCompleted: false),
+        ItemModel(title: "This is the second title!", isCompleted: true),
         ItemModel(title: "Third!", isCompleted: false),
     ]
     var body: some View {
