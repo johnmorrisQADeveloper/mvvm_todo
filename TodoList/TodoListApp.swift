9c90a2c4557c9cfd34d4d6d8a7c8c1ae1c962527
john morris <fennymorris@live.co.uk>
2021-04-25 10:38:38 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by John Morris on 25/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
